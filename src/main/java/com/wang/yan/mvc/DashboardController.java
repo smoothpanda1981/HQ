200f376401bd3c4499fb07748ffbb65b8f9cddd3
smoothpanda1981 <yan.wang.ch@gmail.com>
2015-11-08 15:01:08 +0100

    Add dashboard page

--- src/main/java/com/wang/yan/mvc/DashboardController.java
+++ src/main/java/com/wang/yan/mvc/DashboardController.java
@@ -5,12 +5,46 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@RequestMapping("/")
+@RequestMapping("/dashboard")
 public class DashboardController {
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
+	public String dashBoardPage(ModelMap model) {
+		model.addAttribute("message", "Dashboard");
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/world", "root", "ouafahwafa79");
+
+			DatabaseMetaData md = connection.getMetaData();
+			ResultSet rs = md.getTables(null, null, "%", null);
+			List<String> tableList = new ArrayList<String>();
+			while (rs.next()) {
+				System.out.println(rs.getString(3));
+				tableList.add(rs.getString(3));
+			}
+			model.addAttribute("tableList", tableList);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				model.addAttribute("connection_status", "OK");
+			} else {
+				model.addAttribute("connection_status", "KO");
+			}
+		}
+		return "dashboard";
 	}
 }
--- src/main/webapp/WEB-INF/pages/dashboard.jsp
+++ src/main/webapp/WEB-INF/pages/dashboard.jsp
@@ -1,5 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/xml" prefix="x" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+
 <html>
 <body>
 	<h1>${message}</h1>
+	<br>
+	<c:choose>
+		<c:when test="${connection_status == 'OK'}">
+			mysql : <img src="http://icons.iconarchive.com/icons/dryicons/aesthetica-2/128/database-up-icon.png">
+			<br />
+		</c:when>
+		<c:otherwise>
+			mysql : <img src="http://icons.iconarchive.com/icons/dryicons/aesthetica-2/128/database-down-icon.png">
+			<br />
+		</c:otherwise>
+	</c:choose>
+	<br>
+	<c:forEach items="${tableList}" var="table">
+	Item <c:out value="${table}"/><p>
+		</c:forEach>
 </body>
 </html>
