bb02c24e9a4641b0e16a97eaf77ce7699665b4de
smoothpanda1981 <yan.wang.ch@gmail.com>
2015-12-19 09:55:10 +0100

    Add  @Component

--- src/main/java/com/wang/yan/mvc/HelloController.java
+++ src/main/java/com/wang/yan/mvc/HelloController.java
@@ -1,6 +1,8 @@
 package com.wang.yan.mvc;
 
+import com.wang.yan.mvc.dao.Message;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,13 @@ public class HelloController {
 
 	private static final Logger logger = Logger.getLogger(HelloController.class);
 
+	@Autowired
+	private Message message;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello Yan!");
+
+		model.addAttribute("message", "Hello " + message.getContent() + " !");
 		logger.info("INFO LOG");
 		logger.debug("DEBUG LOG");
 		return "hello";
