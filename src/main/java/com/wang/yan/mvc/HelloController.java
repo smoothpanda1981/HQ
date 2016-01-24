1a15d41382dc4343124155697cd8ee203a6644c4
smoothpanda1981 <yan.wang.ch@gmail.com>
2016-01-24 14:34:43 +0100

    Add RequestMappingHandler

--- src/main/java/com/wang/yan/mvc/HelloController.java
+++ src/main/java/com/wang/yan/mvc/HelloController.java
@@ -7,12 +7,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
 
 	private static final Logger logger = Logger.getLogger(HelloController.class);
+
+	@Autowired
+	private RequestMappingHandlerMapping handlerMapping;
 
 	@Autowired
 	private Message message;
@@ -23,6 +33,10 @@ public class HelloController {
 		model.addAttribute("message", "Hello " + message.getContent() + " !");
 		logger.info("INFO LOG");
 		logger.debug("DEBUG LOG");
+		Map<RequestMappingInfo, HandlerMethod> map = new HashMap<RequestMappingInfo, HandlerMethod>();
+		map = handlerMapping.getHandlerMethods();
+		Set<RequestMappingInfo> keys = map.keySet();
+		model.addAttribute("endPoints", keys);
 		return "hello";
 	}
 }
