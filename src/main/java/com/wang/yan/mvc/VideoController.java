951fa5db7edd8f430193ffffbc7a2bbc325f92a8
smoothpanda1981 <yan.wang.ch@gmail.com>
2016-01-25 01:15:32 +0100

    Video Controler

---
+++ src/main/java/com/wang/yan/mvc/VideoController.java
@@ -0,0 +1,42 @@
+package com.wang.yan.mvc;
+
+import com.wang.yan.mvc.model.Message;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Controller
+@RequestMapping("/")
+public class VideoController {
+
+	private static final Logger logger = Logger.getLogger(VideoController.class);
+
+	@Autowired
+	private RequestMappingHandlerMapping handlerMapping;
+
+	@Autowired
+	private Message message;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+
+		model.addAttribute("message", "Hello " + message.getContent() + " !");
+		logger.info("INFO LOG");
+		logger.debug("DEBUG LOG");
+		Map<RequestMappingInfo, HandlerMethod> map = new HashMap<RequestMappingInfo, HandlerMethod>();
+		map = handlerMapping.getHandlerMethods();
+		Set<RequestMappingInfo> keys = map.keySet();
+		model.addAttribute("endPoints", keys);
+		return "hello";
+	}
+}
