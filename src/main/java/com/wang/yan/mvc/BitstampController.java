de677cb5b8057f5ae1b72a0c57578726a01dcb23
Smooth Panda <smoothpanda1981@gmail.com>
2017-07-06 19:25:35 +0200

    User Pusher, way better

--- src/main/java/com/wang/yan/mvc/BitstampController.java
+++ src/main/java/com/wang/yan/mvc/BitstampController.java
@@ -6,6 +6,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.pusher.client.Pusher;
+import com.pusher.client.PusherOptions;
+import com.pusher.client.channel.Channel;
+import com.pusher.client.channel.SubscriptionEventListener;
+
 @Controller
 @RequestMapping("/bitstamp")
 public class BitstampController {
@@ -14,7 +19,20 @@ public class BitstampController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String bitStampPage(ModelMap model) {
 		model.addAttribute("message", "Bitstamp");
+		PusherOptions options = new PusherOptions();
+		options.setCluster("eu");
+		Pusher pusher = new Pusher("de504dc5763aeef9ff52", options);
+		pusher.connect();
 
+		Channel channel = pusher.subscribe("live_trades");
+		logger.info("YAN YAN");
+		channel.bind("trade", new SubscriptionEventListener() {
+			@Override
+			public void onEvent(String channelName, String eventName, String data) {
+				logger.info("test test test");
+				logger.info(data);
+			}
+		});
 		return "bitstamp";
 	}
 }
