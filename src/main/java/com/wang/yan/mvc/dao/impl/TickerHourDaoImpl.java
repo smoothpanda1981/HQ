5c081c033fc4c2645dc9c333843ee8f38a44aae7
Yan Wang <yan.wang.ch@gmail.com>
2018-03-07 14:36:09 +0100

    minor fix

--- src/main/java/com/wang/yan/mvc/dao/impl/TickerHourDaoImpl.java
+++ src/main/java/com/wang/yan/mvc/dao/impl/TickerHourDaoImpl.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.stream.Collector;
 
 /**
  * Created by ywang on 25.11.15.
@@ -42,4 +45,16 @@ public class TickerHourDaoImpl implements TickerHourDao {
             return null;
         }
     }
+    
+    public static <T> Collector<T, ?, List<T>> lastN(int n) {
+        return Collector.<T, Deque<T>, List<T>>of(ArrayDeque::new, (acc, t) -> {
+            if (acc.size() == n)
+                acc.pollFirst();
+            acc.add(t);
+        }, (acc1, acc2) -> {
+            while (acc2.size() < n && !acc1.isEmpty()) {
+                acc2.addFirst(acc1.pollLast());
+            }
+            return acc2;
+        }, ArrayList<T>::new);}
 }
